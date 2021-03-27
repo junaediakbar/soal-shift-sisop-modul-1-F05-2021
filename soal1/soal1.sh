e5faab26126b35f48a180f3eba1ef332f89a62fc
Raharja214 <72438824+Raharja214@users.noreply.github.com>
2021-03-27 18:24:13 +0700

    Pengerjaan soal 1b dan 1c

--- soal1/soal1.sh
+++ soal1/soal1.sh
@@ -3,3 +3,10 @@
 #soal 1a
  
 log=`cat syslog.log | cut -f6- -d' '`
+
+#soal 1b
+err=`grep "ERROR" "syslog.log" | cut -d' ' -f7- | cut -d'(' -f1 | sort | uniq -c`
+
+#soal 1c
+counterr=`grep "ERROR" "syslog.log"| cut -d'(' -f2 | cut -d')' -f1 | sort | uniq -c`
+countin=`grep "INFO" "syslog.log"| cut -d'(' -f2 | cut -d')' -f1 | sort | uniq -c`
